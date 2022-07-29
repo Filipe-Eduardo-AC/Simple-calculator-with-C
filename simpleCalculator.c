00219740795d544cbf1d40411a1a12017af911ff
Filipe Eduardo Andrade Costa <100179392+Filipe-Eduardo-AC@users.noreply.github.com>
2022-07-29 14:24:12 -0300

    Add files via upload

---
+++ simpleCalculator.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    char operator;
+    double num01;
+    double num02;
+    double result;
+
+    printf("\nEnter an operator (+ - * /): ");
+    scanf("%c", &operator);
+
+    printf("\nEnter a number: ");
+    scanf("%lf", &num01);
+
+    printf("\nEnter another number: ");
+    scanf("%lf", &num02);
+
+    switch (operator)
+    {
+    case '+':
+        result = num01 + num02;
+        printf("\nResult: %.2lf", result);
+        break;
+    case '-':
+        result = num01 - num02;
+        printf("\nResult: %.2lf", result);
+        break;
+    case '*':
+        result = num01 * num02;
+        printf("\nResult: %.2lf", result);
+        break;
+    case '/':
+        result = num01 / num02;
+        printf("\nResult: %.2lf", result);
+        break;
+    default:
+        printf("%c is not valid", operator);
+        break;
+    }
+
+    return 0;
+}
